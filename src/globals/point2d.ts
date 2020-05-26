1f9d775950054e4503256ad49e87b81fc65b6884
Elias <p.elias.graf@gmail.com>
2020-05-26 14:56:15 +0200

    Interface to class

--- src/globals/point2d.ts
+++ src/globals/point2d.ts
@@ -1,4 +1,17 @@
-export interface Point2D {
-	getX(): number;
-	getY(): number;
+export class Point2D {
+	private x: number;
+	private y: number;
+
+	public constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public getX(): number {
+		return this.x;
+	}
+
+	public getY(): number {
+		return this.y;
+	}
 }
