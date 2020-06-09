5bb38d995a89ae9dea9756f7e5c8bebb33c8c521
Elias <p.elias.graf@gmail.com>
2020-06-09 13:09:48 +0200

    Started to implement drawable tetromino

--- src/drawables/drawableTetromino.ts
+++ src/drawables/drawableTetromino.ts
@@ -1,6 +1,28 @@
 import { Drawable } from '../render';
+import { Tetromino } from '../physics/tetromino';
+import { Point2D } from '../globals';
 
-export class DrawableTetromino implements Drawable {
+export class DrawableTetromino implements Drawable, Tetromino {
+	private blocks: Point2D[];
+	private origin: Point2D;
+
+	public constructor(origin: Point2D, template: Point2D[]) {
+		this.origin = origin;
+		this.blocks = template;
+	}
+
+	public getOrigin(): Point2D {
+		return this.origin.clone();
+	}
+	public setOrigin(newOrigin: Point2D): void {
+		this.origin.assign(newOrigin);
+	}
+	public getBlocks(): Point2D[] {
+		throw new Error('Method not implemented.');
+	}
+	public setBlocks(newBlocks: Point2D[]): void {
+		throw new Error('Method not implemented.');
+	}
 	public render(): void {
 		throw new Error('unimplemented');
 	}
