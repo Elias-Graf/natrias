ccde0332c61780d91612e56240e45d6ad142493c
NamikoYa <nati.wernli@gmail.com>
2020-06-09 16:43:24 +0200

    Merge from master

--- src/globals/point2d.ts
+++ src/globals/point2d.ts
@@ -3,11 +3,35 @@ export class Point2D {
 	private y: number;
 
 	/**
+	 * Allows cloning of a specific point
+	 * @param p what point to clone
+	 */
+	public static clone(p: Point2D): Point2D {
+		return new Point2D(p.getX(), p.getY());
+	}
+
+	/**
 	 * Constructor of the Point2D class
 	 */
 	public constructor(x: number, y: number) {
 		this.x = x;
 		this.y = y;
+	}
+
+	/**
+	 * Assigns the values of another point to this one.
+	 * @param point the point of which values will be assigned
+	 */
+	public assign(point: Point2D): void {
+		this.x = point.getX();
+		this.y = point.getY();
+	}
+
+	/**
+	 * Colones the current point instance.
+	 */
+	public clone(): Point2D {
+		return Point2D.clone(this);
 	}
 
 	/**
