715cc8db919df9e431840eccac052e18d69d111b
Elias <p.elias.graf@gmail.com>
2020-06-11 21:41:19 +0200

    Removed dummy tetromino implementation

--- src/physics/physicsEngine.ts
+++ src/physics/physicsEngine.ts
@@ -1,115 +1,11 @@
 import { Dir } from '../globals/direction';
-import { TetrominoInterface } from './tetromino';
-import { Point2D } from '../globals';
+import { Tetromino } from './tetromino';
 import { MoveResponse } from './moveResponse';
 
 /**
- * TODO: Replace calculateRotation function with "rotate" function.
- *  As we're only ever rotating a single step, and only in one direction (not the band).
  * TODO: Move piece into the board if rotated outside it. Currently we just don't allow
  *  rotating.
  */
-
-class Tetromino implements TetrominoInterface {
-	private origin: Point2D;
-	private blocks: Point2D[];
-	private rotation = 0;
-
-	public constructor(origin: Point2D, blocks: Point2D[]) {
-		this.origin = origin;
-		this.blocks = blocks;
-	}
-
-	public static clone(tetromino: Tetromino): Tetromino {
-		const t = new Tetromino(
-			tetromino.getOrigin().clone(),
-			tetromino.getBlocks().map(Point2D.clone)
-		);
-		t.setRotation(tetromino.getRotation());
-		return t;
-	}
-
-	public clone(): Tetromino {
-		return Tetromino.clone(this);
-	}
-
-	public getOrigin(): Point2D {
-		return this.origin;
-	}
-
-	public setOrigin(newOrigin: Point2D): void {
-		this.origin = newOrigin;
-	}
-
-	public getBlocks(): Point2D[] {
-		const newBlocks: Point2D[] = [];
-		this.blocks.forEach((block) => {
-			newBlocks.push(new Point2D(block.getX(), block.getY()));
-		});
-		return newBlocks;
-	}
-
-	public setBlocks(newBlocks: Point2D[]): void {
-		this.blocks = newBlocks;
-	}
-
-	public getRotation(): number {
-		return this.rotation;
-	}
-
-	public setRotation(newRotation: number): void {
-		this.rotation = newRotation;
-	}
-
-	public calculateNextRotation(): number {
-		let nextRotation = this.rotation;
-		if (++nextRotation === 4) {
-			nextRotation = 0;
-		}
-		return nextRotation;
-	}
-
-	public calculateAbsoluteBlocks(): Point2D[] {
-		const originX = this.origin.getX();
-		const originY = this.origin.getY();
-
-		return this.blocks.map((block) => {
-			const blockX = block.getX();
-			const blockY = block.getY();
-			const newBlock = new Point2D(blockX, blockY);
-
-			switch (this.getRotation()) {
-				case 0:
-					break;
-				case 1:
-					newBlock.setX(blockY * -1);
-					newBlock.setY(blockX);
-					break;
-				case 2:
-					newBlock.setX(blockX * -1);
-					newBlock.setY(blockY * -1);
-					break;
-				case 3:
-					newBlock.setX(blockY);
-					newBlock.setY(blockX * -1);
-					break;
-				default:
-					console.error(`Rotation not in limit: ${this.getRotation()}`);
-					break;
-			}
-			newBlock.setX(newBlock.getX() + originX);
-			newBlock.setY(newBlock.getY() + originY);
-			return newBlock;
-		});
-	}
-}
-
-const t = new Tetromino(new Point2D(2, 2), [
-	new Point2D(1, -1),
-	new Point2D(0, -1),
-	new Point2D(0, 0),
-	new Point2D(-1, 0),
-]);
 
 /**
  * Class with the physics of the game
@@ -123,23 +19,7 @@ export class PhysicsEngine {
 	 * Constructor of the PhysicsEngine class
 	 */
 	public constructor() {
-		document.addEventListener('keyup', (event) => {
-			switch (event.key) {
-				case 'ArrowUp':
-					this.rotate(t);
-					break;
-				case 'ArrowRight':
-					this.move(t, Dir.RIGHT);
-					break;
-				case 'ArrowDown':
-					this.move(t, Dir.DOWN);
-					break;
-				case 'ArrowLeft':
-					this.move(t, Dir.LEFT);
-					break;
-			}
-		});
-
+		// Initialize board
 		for (let y = 0; y < 20; y++) {
 			this.board.push([]);
 			for (let x = 0; x < 10; x++) {
@@ -147,9 +27,6 @@ export class PhysicsEngine {
 				else this.board[y][x] = false;
 			}
 		}
-
-		this.showDebugInterface();
-		this.setTetromino(t, true);
 	}
 
 	/**
@@ -163,7 +40,7 @@ export class PhysicsEngine {
 	/**
 	 * Method that creates Tetromino
 	 */
-	private setTetromino(tetromino: TetrominoInterface, isActive: boolean): void {
+	private setTetromino(tetromino: Tetromino, isActive: boolean): void {
 		tetromino.calculateAbsoluteBlocks().forEach((block) => {
 			this.board[block.getY()][block.getX()] = isActive;
 		});
@@ -188,7 +65,7 @@ export class PhysicsEngine {
 	/**
 	 * Method that lets tetromino turn
 	 */
-	private rotate(tetromino: TetrominoInterface): void {
+	private rotate(tetromino: Tetromino): void {
 		const newTetromino = tetromino.clone();
 		newTetromino.setRotation(newTetromino.calculateNextRotation());
 
@@ -219,7 +96,7 @@ export class PhysicsEngine {
 	/**
 	 * Method that lets tetromino move
 	 */
-	public move(tetromino: TetrominoInterface, dir: Dir): MoveResponse {
+	public move(tetromino: Tetromino, dir: Dir): MoveResponse {
 		const newTetromino = tetromino.clone();
 		const newOrigin = newTetromino.getOrigin();
 
@@ -247,8 +124,8 @@ export class PhysicsEngine {
 	 * Method that checks collision of tetromino with other blocks when moving
 	 */
 	private isColliding(
-		oldTetromino: TetrominoInterface,
-		newTetromino: TetrominoInterface
+		oldTetromino: Tetromino,
+		newTetromino: Tetromino
 	): boolean {
 		for (const block of newTetromino.calculateAbsoluteBlocks()) {
 			if (
@@ -268,7 +145,7 @@ export class PhysicsEngine {
 	/**
 	 * Method that checks if coordinates are out of bounds
 	 */
-	private outOfBounds(tetromino: TetrominoInterface): boolean {
+	private outOfBounds(tetromino: Tetromino): boolean {
 		for (const block of tetromino.calculateAbsoluteBlocks()) {
 			if (
 				block.getX() > PhysicsEngine.WIDTH - 1 ||
