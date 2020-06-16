5d9f43c5d56e0f2797363f1fa74456186ad3a766
NamikoYa <nati.wernli@gmail.com>
2020-06-16 12:50:02 +0200

    remove calculateNextRotation, added getType and rotate

--- src/physics/tetromino.ts
+++ src/physics/tetromino.ts
@@ -1,4 +1,5 @@
 import { Point2D } from '../globals';
+import { TemplateType } from '../tetrominoes/type';
 
 /**
  * TODO: Replace calculateRotation function with "rotate" function.
@@ -9,9 +10,10 @@ export interface Tetromino {
 	setOrigin(newOrigin: Point2D): void;
 	getBlocks(): Point2D[];
 	setBlocks(newBlocks: Point2D[]): void;
+	getType(): TemplateType;
+	rotate(): void;
 	getRotation(): number;
 	setRotation(newRotation: number): void;
-	calculateNextRotation(): number;
 	calculateAbsoluteBlocks(): Point2D[];
 	clone(): Tetromino;
 }
