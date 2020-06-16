7cdc8263e5318a99cc9d9aed7c7da10f157aeeb3
Elias <p.elias.graf@gmail.com>
2020-06-16 13:46:33 +0200

    Added methods "setBlocks" and "removeFullLines"

--- src/physics/interface.ts
+++ src/physics/interface.ts
@@ -1,8 +1,11 @@
 import { Dir } from '../globals';
 import { Tetromino } from './tetromino';
 import { MoveResponse } from './moveResponse';
+import { Block } from './block';
 
 export interface PhysicsInterface {
 	move(tetromino: Tetromino, dir: Dir): MoveResponse;
+	removeFullLines(): void;
 	rotate(tetromino: Tetromino): void;
+	setBlocks(blocks: Block[]): void;
 }
