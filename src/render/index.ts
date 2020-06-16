93f3b9571a129bca33b37d09a27686b4eccc54e9
Elias <p.elias.graf@gmail.com>
2020-06-16 15:54:45 +0200

    Added default drawable

--- src/render/index.ts
+++ src/render/index.ts
@@ -1,3 +1,4 @@
+export { DefaultDrawable } from './defaultDrawable';
 export { Drawable } from './drawable';
 export { RenderEngine } from './renderEngine';
 export { RendererInterface } from './interface';
