5f0a7de4a3b63b80ba8c6e04a61a893bf677de34
Elias <p.elias.graf@gmail.com>
2020-06-17 16:33:48 +0200

    Sorted interface

--- src/physics/tetromino.ts
+++ src/physics/tetromino.ts
@@ -1,28 +1,16 @@
-import { Point2D } from '../globals';
+import { Point2D, Dir } from '../globals';
 import { TemplateType } from '../tetrominoes/type';
+import { Block } from './block';
 
-/**
- * TODO: Replace calculateRotation function with "rotate" function.
- * As we're only ever rotating a single step, and only in one direction (not the band).
- */
 export interface Tetromino {
-	/**
-	 * Gets the type of the tetromino
-	 */
-	getType(): TemplateType;
-	/**
-	 * Rotates the tetromino by 90deg
-	 */
-	rotate(): void;
-	/**
-	 * Calculates and returns the blocks of the tetromino WITH ABSOLUTE COORDINATES
-	 * NOT THE TEMPLATE (which is relative)
-	 */
-	calculateAbsoluteBlocks(): Point2D[];
 	/**
 	 * Clones the tetromino
 	 */
 	clone(): Tetromino;
+	/**
+	 * Returns the blocks of that the tetromino is made up of
+	 */
+	getBlocks(): Block[];
 	/**
 	 * Get the origin of the tetromino
 	 */
@@ -31,6 +19,19 @@ export interface Tetromino {
 	 * Get the rotation of the tetromino
 	 */
 	getRotation(): number;
+	/**
+	 * Gets the type of the tetromino
+	 */
+	getType(): TemplateType;
+	/**
+	 * Move the tetromino in the desired direction
+	 * @param direction in what direction should the tetromino move
+	 */
+	move(direction: Dir): void;
+	/**
+	 * Rotates the tetromino by 90deg
+	 */
+	rotate(amount?: number): void;
 	/**
 	 * Set the origin of the tetromino
 	 * @param origin new origin
