f41a4bbd7d1c6934b3fb64382286fb744d339544
Elias <p.elias.graf@gmail.com>
2020-06-17 16:33:23 +0200

    Added add, flip, multiply and scale method

--- src/globals/point2d.ts
+++ src/globals/point2d.ts
@@ -9,7 +9,6 @@ export class Point2D {
 	public static clone(p: Point2D): Point2D {
 		return new Point2D(p.getX(), p.getY());
 	}
-
 	/**
 	 * Prints out "Point2D(x, y)"
 	 * @param p what point to stringify
@@ -17,7 +16,6 @@ export class Point2D {
 	public static toString(p: Point2D): string {
 		return `Point2D(${p.getX()}, ${p.getY()})`;
 	}
-
 	/**
 	 * Constructor of the Point2D class
 	 */
@@ -25,69 +23,100 @@ export class Point2D {
 		this.x = x;
 		this.y = y;
 	}
+	/**
+	 * Add a point to the current point
+	 * @param point point object which should be added
+	 */
+	public add(point: Point2D): Point2D {
+		this.x += point.getX();
+		this.y += point.getY();
 
+		return this;
+	}
 	/**
 	 * Assigns the values of another point to this one.
 	 * @param point the point of which values will be assigned
 	 */
-	public assign(point: Point2D): void {
+	public assign(point: Point2D): Point2D {
 		this.x = point.getX();
 		this.y = point.getY();
-	}
 
+		return this;
+	}
+	/**
+	 * Flip the two coordinates
+	 */
+	public flip(): Point2D {
+		const tempX = this.x;
+
+		this.x = this.y;
+		this.y = tempX;
+
+		return this;
+	}
 	/**
 	 * Colones the current point instance.
 	 */
 	public clone(): Point2D {
 		return Point2D.clone(this);
 	}
-
 	/**
 	 * Method that gets x-coordinate
 	 */
 	public getX(): number {
 		return this.x;
 	}
-
 	/**
 	 * Method that gets y-coordinate
 	 */
 	public getY(): number {
 		return this.y;
 	}
+	/**
+	 * Multiply this point with another
+	 * @param point point with which to multiply with
+	 */
+	public multiply(point: Point2D): Point2D {
+		this.x *= point.getX();
+		this.y *= point.getY();
 
-	public multiply(x: number, y: number): void {
-		this.x *= x;
-		this.y *= y;
+		return this;
 	}
-
-	public scale(scale: number): void {
+	/**
+	 * Multiply both coordinates with a factor
+	 * @param scale factor
+	 */
+	public scale(scale: number): Point2D {
 		this.x *= scale;
 		this.y *= scale;
-	}
 
+		return this;
+	}
 	/**
 	 * Method that sets x-coordinate
 	 */
-	public setX(x: number): void {
+	public setX(x: number): Point2D {
 		this.x = x;
-	}
 
+		return this;
+	}
 	/**
 	 * Method that sets y-coordinate
 	 */
-	public setY(y: number): void {
+	public setY(y: number): Point2D {
 		this.y = y;
-	}
 
+		return this;
+	}
 	/**
 	 * Method that sets both x- and y-coordinate
 	 */
-	public setXY(x: number, y: number): void {
+	public setXY(x: number, y: number): Point2D {
 		this.x = x;
 		this.y = y;
-	}
 
+		return this;
+	}
 	/**
 	 * Calls the static toString method.
 	 * @see Point2D.toString
