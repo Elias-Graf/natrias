5ed6cd325380122f6c38d5af09e96b338959116f
Elias <p.elias.graf@gmail.com>
2020-06-17 16:33:35 +0200

    Added opposite function

--- src/globals/direction.ts
+++ src/globals/direction.ts
@@ -1,6 +1,16 @@
 export enum Dir {
-	UP,
-	RIGHT,
 	DOWN,
 	LEFT,
+	RIGHT,
+	UP,
+}
+
+export namespace Dir {
+	export function opposite(direction: Dir): Dir {
+		if (direction === Dir.DOWN) return Dir.UP;
+		if (direction === Dir.LEFT) return Dir.RIGHT;
+		if (direction === Dir.RIGHT) return Dir.LEFT;
+		if (direction === Dir.UP) return Dir.DOWN;
+		throw new Error(`Direction "${direction}"`);
+	}
 }
