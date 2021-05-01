e659027c429a2878e23ca41e4e5013a268a003b6
Elias <p.elias.graf@gmail.com>
2021-05-01 11:20:40 +0200

    Removed sub module and installed as package

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
-	devtool: 'inline-source-map',
+	devtool: "inline-source-map",
 	devServer: {
-		contentBase: path.join(__dirname, './src/client'),
+		contentBase: path.join(__dirname, "./src/client"),
 		port: 9000,
 		watchContentBase: true,
-		host: '0.0.0.0',
+		host: "0.0.0.0",
 		hot: true,
 	},
-	entry: './src/client/index.ts',
-	mode: 'development',
+	entry: "./src/client/index.ts",
+	mode: "development",
 	module: {
 		rules: [
 			{
@@ -22,12 +22,12 @@ module.exports = {
 		],
 	},
 	resolve: {
-		extensions: ['.ts', '.js'],
-		plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })]
+		extensions: [".js", ".ts"],
+		plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
 	},
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'build'),
-		publicPath: '/build/',
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "build"),
+		publicPath: "/build/",
 	},
 };
