c0110cf3223c140003ce540590d6fd9361ee4f05
Elias <p.elias.graf@gmail.com>
2021-05-21 18:41:28 +0200

    Added function to debug template. Also fixed the J and L template using it.

--- src/server/game/templates.ts
+++ src/server/game/templates.ts
@@ -9,16 +9,16 @@ const TEMPLATE_I: ReadonlyVector2[] = [
 	new Vector2(0, -1),
 ];
 const TEMPLATE_J: ReadonlyVector2[] = [
+	new Vector2(0, -1),
+	new Vector2(0, 0),
+	new Vector2(0, 1),
+	new Vector2(-1, 1),
+];
+const TEMPLATE_L: ReadonlyVector2[] = [
+	new Vector2(0, -1),
 	new Vector2(0, 0),
 	new Vector2(0, 1),
 	new Vector2(1, 1),
-	new Vector2(1, 2),
-];
-const TEMPLATE_L: ReadonlyVector2[] = [
-	new Vector2(0, -2),
-	new Vector2(0, -1),
-	new Vector2(0, 0),
-	new Vector2(1, 0),
 ];
 const TEMPLATE_O: ReadonlyVector2[] = [
 	new Vector2(0, -1),
@@ -56,3 +56,67 @@ const TEMPLATES: Record<TetrominoType, ReadonlyVector2[]> = {
 };
 
 export default TEMPLATES;
+
+/**
+ * Prints the template to the console, allowing one to easily debug it.
+ * @param template template array.
+ * @param type Optionally provide the tetromino/template type with will be
+ * added to the log, providing more information.
+ */
+export function debugTemplate(
+	template: ReadonlyVector2[],
+	type?: TetrominoType
+): void {
+	let maxX = 0;
+	let maxY = 0;
+	let minX = 0;
+	let minY = 0;
+
+	for (const { x, y } of template) {
+		if (x > maxX) maxX = x;
+		if (y > maxY) maxY = y;
+		if (x < minX) minX = x;
+		if (y < minY) minY = y;
+	}
+
+	// The "0" block is also a block, thus + 1.
+	const width = maxX - minX + 1;
+	const height = maxY - minY + 1;
+	const offset = new Vector2(minX, minY);
+	const board = new Array(height)
+		.fill(null)
+		.map(() => new Array(width).fill(null).map(() => "o"));
+
+	for (const block of template) {
+		const { x, y } = block.clone.sub(offset);
+
+		try {
+			board[y][x] = "x";
+		} catch (e) {
+			console.log(x, y, e);
+		}
+	}
+
+	print([
+		`Type: \t\t${type === undefined ? "<not provided>" : TetrominoType[type]}`,
+		`Width: \t\t${width}`,
+		`Height: \t${height}`,
+		`Visualized template: \n\t${board.map((row) => row.join("")).join("\n\t")}`,
+	]);
+
+	function print(lines: string[]) {
+		let longest = 0;
+
+		for (const line of lines) {
+			if (line.length > longest) longest = line.length;
+		}
+
+		separator(longest);
+		console.log(lines.join("\n"));
+		separator(longest);
+
+		function separator(length: number) {
+			console.log("*".repeat(length));
+		}
+	}
+}
